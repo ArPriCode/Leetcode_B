a84f2c3e6f7bf1ed3a760e340f7d63271cd00476
Arun Giri <atulgiri649@gmail.com>
2026-07-05 09:19:28 +0530

    Time: 91 ms (44.74%), Space: 15.9 MB (89.47%) - LeetHub

---
+++ 0497-random-point-in-non-overlapping-rectangles/0497-random-point-in-non-overlapping-rectangles.py
@@ -0,0 +1,24 @@
+import random
+import bisect
+
+class Solution(object):
+
+    def __init__(self, rects):
+        self.rects = rects
+        self.prefix = []
+        total = 0
+
+        for x1, y1, x2, y2 in rects:
+            total += (x2 - x1 + 1) * (y2 - y1 + 1)
+            self.prefix.append(total)
+
+    def pick(self):
+        k = random.randint(1, self.prefix[-1])
+        i = bisect.bisect_left(self.prefix, k)
+
+        x1, y1, x2, y2 = self.rects[i]
+
+        x = random.randint(x1, x2)
+        y = random.randint(y1, y2)
+
+        return [x, y]
