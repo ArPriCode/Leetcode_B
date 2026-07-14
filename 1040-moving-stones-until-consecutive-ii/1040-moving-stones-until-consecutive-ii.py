172266ea092e10abf27abc8412b2486e3094166c
Arun Giri <atulgiri649@gmail.com>
2026-07-14 08:38:55 +0530

    Time: 23 ms (61.54%), Space: 13.1 MB (69.23%) - LeetHub

--- 1040-moving-stones-until-consecutive-ii/1040-moving-stones-until-consecutive-ii.py
+++ 1040-moving-stones-until-consecutive-ii/1040-moving-stones-until-consecutive-ii.py
@@ -1,17 +1,23 @@
-class Solution:
-    def numMovesStonesII(self, stones: List[int]) -> List[int]:
-        
+class Solution(object):
+    def numMovesStonesII(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: List[int]
+        """
         stones.sort()
-        n, left, lo = len(stones), 0, inf
+        n = len(stones)
 
-        hi = max(stones[n-1]-stones[1], stones[n-2]-stones[0]) - n + 2
-        
+        maxMoves = max(stones[-1] - stones[1] - (n - 2), stones[-2] - stones[0] - (n - 2))
+
+        minMoves = n  
+        left = 0
         for right in range(n):
+            while stones[right] - stones[left] + 1 > n:
+                left += 1
+            current_window_size = right - left + 1
+            if current_window_size == n - 1 and stones[right] - stones[left] + 1 == n - 1:
+                minMoves = min(minMoves, 2) 
+            else:
+                minMoves = min(minMoves, n - current_window_size)
 
-            left= bisect_right(stones,stones[right]-n)
-
-            if right-left == stones[right]-stones[left] == n-2: lo = min(lo, 2)
-            
-            else: lo = min(lo, n - (right-left+1))
-
-        return [lo, hi]
+        return [minMoves, maxMoves]
