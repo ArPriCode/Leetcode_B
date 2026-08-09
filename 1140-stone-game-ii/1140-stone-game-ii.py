74763dc17d2eb448fe68a42adccfaee8e2801268
Arun Giri <atulgiri649@gmail.com>
2026-08-09 11:42:21 +0530

    Time: 65 ms (95.79%), Space: 26.1 MB (49.15%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.py
+++ 1140-stone-game-ii/1140-stone-game-ii.py
@@ -1,29 +1,13 @@
-class Solution(object):
-    def stoneGameII(self, piles):
-        n = len(piles)
+class Solution:
+    def stoneGameII(self, piles: List[int]) -> int:
+        for i in range(len(piles) - 2, -1, -1):
+            piles[i] += piles[i + 1]
 
-        # suffix[i] = total stones from i to end
-        suffix = [0] * (n + 1)
-        for i in range(n - 1, -1, -1):
-            suffix[i] = suffix[i + 1] + piles[i]
-
-        memo = {}
-
+        @cache
         def dfs(i, M):
-            if i >= n:
-                return 0
+            if i + M * 2 >= len(piles):
+                return piles[i]
 
-            if 2 * M >= n - i:
-                return suffix[i]
-
-            if (i, M) in memo:
-                return memo[(i, M)]
-
-            best = 0
-            for X in range(1, 2 * M + 1):
-                best = max(best, suffix[i] - dfs(i + X, max(M, X)))
-
-            memo[(i, M)] = best
-            return best
+            return piles[i] - min(dfs(i + j, max(M, j)) for j in range(1, M * 2 + 1))
 
         return dfs(0, 1)
