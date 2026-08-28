8f92474f5b56d1ff06c33e7d25a82034e55ab6cf
Arun Giri <atulgiri649@gmail.com>
2026-08-28 09:03:36 +0530

    Time: 87 ms (15.32%), Space: 29.6 MB (16.67%) - LeetHub

---
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxWidthRamp(self, nums):
+        ans = 0
+        n = len(nums)
+        vp = [(nums[i], i) for i in range(n)]
+        vp.sort()
+        min_index = vp[0][1]
+        for i in range(1, n):
+            current_index = vp[i][1]
+            ans = max(ans, current_index - min_index)
+            min_index = min(min_index, current_index)
+
+        return ans
